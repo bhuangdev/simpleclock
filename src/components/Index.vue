3939a1216e3b7b0d58c2f2f2d1a41e3eb784431e
bhuang <robinloveslife@163.com>
2021-04-11 21:22:09 +0800

    upload

---
+++ src/components/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <div class="date">{{ date }}</div>
+    <div class="time">{{ time }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      date: '',
+      time: ''
+    }
+  },
+  mounted(){
+    var that = this;
+    setInterval(
+      function(){ 
+        that.showTime(); 
+      }, 1000
+    );
+  },
+  methods:{
+    showTime(){
+      var myDate = new Date(); //实例一个时间对象；
+      var year = myDate.getFullYear();   //获取系统的年；
+      var month = myDate.getMonth()+1;   //获取系统月份，由于月份是从0开始计算，所以要加1
+      var day = myDate.getDate(); // 获取系统日，
+      var hour = myDate.getHours(); //获取系统时，
+      var minute = myDate.getMinutes(); //分
+      var second = myDate.getSeconds(); //秒
+      if(Number(second) < 10){
+        second = '0' + second;
+      }
+      this.date = month + '月' + day + '日';
+      this.time = hour + ':' + minute + ':' + second
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello{
+  left:50px;
+  right:50px;
+  margin-top:5%;
+}
+.date{
+  font-size:200px;
+}
+.time{
+  font-size:250px;
+}
+</style>
